97c96597b19f992045b9b0282efa67254583f2e9
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-23 20:36:17 +0800

    202507050418 api 3

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,3 @@
-// api/hello.js
-module.exports = (req, res) => {
+export default (req, res) => { // 將 module.exports 替換為 export default
   res.status(200).send('Hello from Vercel Serverless Function!');
 };
